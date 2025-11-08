e18f6ab318cc68a54e0f72549938f0d3a7e50d8d
Omshree Kenjale <omshree.22311580@viit.ac.in>
2025-11-08 15:50:50 +0530

    port 3000

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,4 +9,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    port: 3000, // 👈 Custom port
+    open: true, // (Optional) Automatically opens the browser
+  },
 })
